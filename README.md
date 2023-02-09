b551601c843f40c42db04c787a6d934152458262
Karthik Koppaka <karthik@officialk.dev>
2023-02-09 11:38:43 +0530

    feat(mvp):added sorting feature

--- README.md
+++ README.md
@@ -20,17 +20,47 @@
 
 ## Installation
 
-```cmd
-yarn add depensort
+```yarn
+yarn add depensort --dev
 ```
 
-```cmd
-npm install depensort
+```npm
+npm i -D depensort
 ```
 
 ## Usage
 
-[Refer to the Documentation](https://npm.officialk.dev/depensort)
+The package is used to sort the dependencies in the package.json file in ascending order of key length after every install(or on command)
+
+### 1. Set the package to run after every install
+
+```json
+"scripts":{
+	"postinstall":"depensort"
+}
+```
+
+### 2. Call it manually
+
+**From the cmd**
+
+-   Install the package globally
+
+```npm
+npm i -g depensort
+```
+
+OR
+
+```yarn
+yarn global add depensort
+```
+
+-   Call it from the cmd
+
+```bash
+depensort
+```
 
 ## Contributing
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { readFileSync } from "fs"
+import File from "./File"
+import Sorter from "./Sorter"
 
-import sayHello from "./hello/index"
-
-const author = JSON.parse(readFileSync("package.json", { encoding: "utf8" }) ?? "{}")?.author?.name ?? "officialk"
-
-// eslint-disable-next-line no-console
-console.log(sayHello(author))
-
-export default sayHello
+export default (() => {
+	const packageFile = new File("./package.json")
+	const packageValues = packageFile.read()
+	if (packageValues.dependencies) {
+		packageValues.dependencies = new Sorter(packageValues.dependencies).sort()
+	}
+	if (packageValues.devDependencies) {
+		packageValues.devDependencies = new Sorter(packageValues.devDependencies).sort()
+	}
+	if (packageValues.resolutions) {
+		packageValues.resolutions = new Sorter(packageValues.resolutions).sort()
+	}
+	if (packageValues.overrides) {
+		packageValues.overrides = new Sorter(packageValues.overrides).sort()
+	}
+	packageFile.write(packageValues)
+})()
