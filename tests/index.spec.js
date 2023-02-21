d5b28d88e0ad19a1122da6c16f32459d80386ef9
officialk <karthik@officialk.dev>
2023-02-21 23:46:37 +0530

    test(package): updated the integration/package level tests to be more realistic
    updated the tests folder to now use the package link to actually test the package workings in a
    deployed env

---
+++ tests/index.spec.js
@@ -0,0 +1,24 @@
+const { execSync } = require("child_process")
+const { readFileSync, writeFileSync } = require("fs")
+let contents
+describe("Testing the working of the package", () => {
+	beforeAll(() => {
+		contents = JSON.parse(readFileSync("package.json", "utf8"))
+	})
+	afterAll(() => {
+		writeFileSync("package.json", JSON.stringify(contents, null, 4), "utf8")
+	})
+	it("should sort package json dependencies", () => {
+		execSync("yarn run sort")
+		const dependencies = contents.dependencies
+		const sortedArray = Object.entries(dependencies).sort((first, second) => {
+			let difference = first[0].length - second[0].length
+			if (difference === 0) {
+				difference = first[1].length - second[1].length
+			}
+			return difference
+		})
+		const newFile = JSON.parse(readFileSync("package.json", "utf8"))
+		expect(JSON.stringify(Object.fromEntries(sortedArray))).toBe(JSON.stringify(newFile.dependencies))
+	})
+})
